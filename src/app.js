266c880fedc796cb95e4f4b7b41894dbd08c686b
stroligo <gabrielstroligo@gmail.com>
2020-04-10 03:44:14 +0100

    Correcao com testes validados

--- src/app.js
+++ src/app.js
@@ -1,99 +1,73 @@
 const express = require("express");
-const { uuid, isUuid } = require('uuidv4'); 
 const cors = require("cors");
+const { uuid } = require('uuidv4'); 
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use(logRequests); 
-app.use('/repositories/:id', validadeRepositoriesId); 
-
-//VAR PRINCIPAL
-const repositories = [];
+const repositories = [ ];
 
 
-function logRequests(request, response, next) {
-  const { method, url} = request;
-  const logLabel = `[${method.toUpperCase()}] ${url}`;
-  console.time(logLabel); 
-  next(); 
-  console.timeEnd(logLabel); 
-};
-// VALIDA ID
-function validadeRepositoriesId(request, response, next){
-  const { id } = request.params;
-  if(!isUuid(id)){
-    return response.status(400).json({error: 'Invalid repositorories ID'});
-  }
-  return next();
-};
-//------------------------------
-
-// METHOD GET
 app.get("/repositories", (request, response) => {
-  const {title} = request.query;
-  const results = title
-    ? repositories.filter(repository => repository.title.includes(title))
-    : repositories;
-  console.log('Requested listing');
-  return response.json(results); 
+   return response.json(repositories); 
 });
-// METHOD POST
+
 app.post("/repositories", (request, response) => {
-  const {title , url, techs, likes}= request.body;
-  const repository = { id: uuid(), title, url, techs, likes};
+  const {title , url, techs }= request.body;
+  const repository = { 
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
   repositories.push(repository);  
-  console.log('New repository added');
   return response.json(repository); 
 });
-// METHOD PUT
+
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params; 
   const {title , url, techs}= request.body;
-  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
-  
-  if(repositoryIndex < 0){
-    console.log('Modification denied');
-     return response.status(400).json({ error: 'Repository not found'}) 
-     }
-  const repository = { id, title, url, techs};
-  repositories[repositoryIndex] = repository; 
-  console.log('Modification accepted');
+   const findRepositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if(findRepositoryIndex === -1){
+    return response.status(400).json({ error: 'Repository not found'}) ;
+  };
+   const repository= {
+     id,
+     title,
+     url,
+     techs,
+     likes: repositories[findRepositoryIndex].likes,
+   }; 
+   repositories[findRepositoryIndex] = repository;  
   return response.json(repository); 
 });
 
-// METHOD DELETE
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
-  
-  if(repositoryIndex < 0){
+  const findRepositoryIndex = repositories.findIndex(repository => repository.id === id);  
+  if(findRepositoryIndex > 0){
+    repositories.splice(findRepositoryIndex, 1);
+  }else {
      return response.status(400).json({ error: 'Repository not found'})
-     }
-
-  repositories.splice(repositoryIndex, 1); 
-  console.log('Deleted repository');
+     };
   return response.status(204).send(); 
 });
 
-// METHOD POST LIKE 
-app.post("/repositories/:id/likes", (request, response) => {
-  const {id} = request.params; 
-  const {likes} = request.query;
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params; 
 
-  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
-    if(repositoryIndex < 0){
-    console.log('Modification denied');
-     return response.status(400).json({ error: 'Repository not found'}) 
-     };
+  const findRepository = repositories.find(repository => repository.id === id);  
+  
+  if(!findRepository){
+    return response.status(400).json({ error: 'Repository does not exists.'});
+  }
 
-     let totalLikes = parseInt(repositories[repositoryIndex].likes);
-     totalLikes++;
-     repositories[repositoryIndex].likes = totalLikes;       
-  console.log('Likes ADD');
-  return response.json(totalLikes); 
+  findRepository.likes += 1;  
+  
+  return response.json(findRepository); 
 });
 
 module.exports = app; 
